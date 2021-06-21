1937d895fa7cbc3cb8e862a05208b256edacdf20
sudo-Cthulhufhtagn <aleksandrmakarov2901@gmail.com>
2021-06-21 09:13:14 +0300

    speedster

---
+++ scripts/speed_sniffer.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+#***********************************************************
+#* Software License Agreement (BSD License)
+#*
+#*  Copyright (c) 2009, Willow Garage, Inc.
+#*  All rights reserved.
+#*
+#*  Redistribution and use in source and binary forms, with or without
+#*  modification, are permitted provided that the following conditions
+#*  are met:
+#*
+#*   * Redistributions of source code must retain the above copyright
+#*     notice, this list of conditions and the following disclaimer.
+#*   * Redistributions in binary form must reproduce the above
+#*     copyright notice, this list of conditions and the following
+#*     disclaimer in the documentation and/or other materials provided
+#*     with the distribution.
+#*   * Neither the name of the Willow Garage nor the names of its
+#*     contributors may be used to endorse or promote products derived
+#*     from this software without specific prior written permission.
+#*
+#*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#*  POSSIBILITY OF SUCH DAMAGE.
+#***********************************************************
+
+# Author: Blaise Gassend
+
+import rospy, os, sys, rospkg
+from sound_play.msg import SoundRequest
+from geometry_msgs.msg import Twist
+from sound_play.libsoundplay import SoundClient
+
+def sleep(t):
+    try:
+        rospy.sleep(t)
+    except:
+        pass
+
+def callback(data):
+        # print(f'Lin x: {data.linear.x}')
+        # print(f'Lin y: {data.linear.y}')
+        # print(f'Lin z: {data.linear.z}')
+        # print(f'Ang x: {data.angular.x}')
+        # print(f'Ang y: {data.angular.y}')
+        # print(f'Ang z: {data.angular.z}')
+        speeds=[data.linear.x, data.linear.y, data.linear.z]
+        for i in speeds:
+            if i>5:
+                player("Maximum_speed_achieved.ogg")
+                break
+
+def player(melody_name):
+    global to_files, soundhandle
+    soundhandle.stopAll()
+    soundhandle.playWave(to_files + melody_name)
+    sleep(2)
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('soundplay_test', anonymous = True)
+    global soundhandle
+    soundhandle = SoundClient()
+
+    rospy.sleep(1)
+
+    soundhandle.stopAll()
+    global to_files
+    rospack = rospkg.RosPack()
+    to_files=rospack.get_path('music_box')+'/audio/'
+
+    rospy.loginfo('Attempt 1')
+    soundhandle.playWave(to_files + "System_initialized.ogg")
+    sleep(2)
+    rospy.loginfo('Attempt 2')
+    soundhandle.playWave(to_files + "Systems_operating_at_maximum.ogg")
+    sleep(2)
+    # t = rospy.Timer(rospy.Duration(0.1), timer_callback)
+    rospy.Subscriber("cmd_vel", Twist, callback)
+    rospy.spin()

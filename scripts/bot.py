fa2e0f39d93ab2f3028b0333d62c1b4440e86012
sudo-Cthulhufhtagn <sudo-Cthulhufhtagn>
2021-06-22 12:51:50 +0300

    Fixes

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy, os, sys, rospkg, telebot
 from sound_play.msg import SoundRequest
+from geometry_msgs.msg import Twist
 bot = telebot.TeleBot('1763023661:AAHtQBxpg0QGWIN-a4G67IhRY9Yx5intVz8')
 
 from sound_play.libsoundplay import SoundClient
@@ -14,6 +15,11 @@ def sleep(t):
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+
+def timer_callback(event):
+	global last_heartbeat
+	if (rospy.get_time() - last_heartbeat) >= 0.5:
+		cmd_vel_pub.publish(Twist())
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -42,7 +48,7 @@ if __name__ == '__main__':
     rospy.init_node('telegram_bot', anonymous = True)
     soundhandle = SoundClient()
     global cmd_vel_pub
-	cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     t = rospy.Timer(rospy.Duration(0.1), timer_callback)
     rospy.sleep(1)
 
